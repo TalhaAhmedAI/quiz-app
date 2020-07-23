1955ebbcfcf118bc1629dd8f12be48d52d6b6860
tester <aiforeveryoneai@gmail.com>
2020-07-23 18:31:58 +0500

    Added some components & images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+//Components
+import QuestionCard from "./components/questionCard";
 
 function App() {
+  const startQuiz = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startQuiz}></button>
+      <p className="score">Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
