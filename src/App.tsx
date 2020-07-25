07cfae5965cbd8faaf74c43ef126dafd8248ed68
tester <aiforeveryoneai@gmail.com>
2020-07-25 07:26:05 +0500

    Update App.tsx
    Added some logic to <next Question> button so it disappears if all questions have been answered.

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,16 @@ const App: React.FC = () => {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
   return (
     <div className="App">
       <h1>React Quiz</h1>
